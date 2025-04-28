7565b8fb161c2d9fb6df8cce6d36342949c50175
Adeethyia Shankar <adeethyia_shankar@brown.edu>
2025-04-28 16:01:34 -0400

    Save biological time to file

--- STDP_Recurrent.py
+++ STDP_Recurrent.py
@@ -854,7 +854,8 @@ def run_stdp(setup, run_length, dt=0.0005, L_analysis=5.0, w_distr_prctile=99, f
         if abs(((T_prev + t) * dt) % 1) < 1e-10:
             w_EE = np.clip(w_EE, 0, wEEmax) # Clip weights
         if abs(((T_prev + t) * dt) % 10) < 1e-10:
-            print(f"{(T_prev + t)*dt:.1f} sec") # Print biological time
+            with open('../stdp_time_counter', 'a'):
+                print(f"{(T_prev + t)*dt:.1f} sec") # Print biological time
     
     total_time += run_length
 
